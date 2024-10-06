54846d83452bdb379919b3e283661acbabe3fa62
Dhanush2002-28 <dhanushchandru28@gmail.com>
2024-10-06 23:58:32 +0530

    Performance upgrade

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 AOS.init({
-  duration: 800,
-  once: true,
+  offset: 0, 
+  delay:0,  // Trigger animations later in the scroll
+  duration: 100, // Fast animations
+  easing: 'ease-in-out',
+  once: true,    // Only animate once
+  throttleDelay:  // Less frequent scroll checks // Higher value for less frequent scroll checks
+  
+  
 });
+
+
+
