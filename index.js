875ad25b860a6f112d507d257b93643de12bbf71
Dhanush2002-28 <dhanushchandru28@gmail.com>
2024-10-07 00:02:23 +0530

    Performance upgrade

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-window.alert("Please view it in Desktop/Laptop for best experience")
+window.onload = function () {
+  alert("Please view this webiste in desktop/laptop only for best experince.")
+}
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 AOS.init({
   offset: 0, 
-  delay:0,  // Trigger animations later in the scroll
-  duration: 100, // Fast animations
+  delay:0,  
+  duration: 100, 
   easing: 'ease-in-out',
-  once: true,    // Only animate once
-  throttleDelay:  // Less frequent scroll checks // Higher value for less frequent scroll checks
-  
-  
+  once: true,    
+  throttleDelay:  
 });
+
+
 
 
 
