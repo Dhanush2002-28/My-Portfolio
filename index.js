1b8e89dac7b96b9f6b6e3c3ce5795608edb12d01
Dhanush2002-28 <dhanushchandru28@gmail.com>
2024-10-09 10:30:13 +0530

    Added back to top button and feature

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ AOS.init({
   throttleDelay:  
 });
 
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+
+window.onscroll = function() { scrollFunction(); };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+
+backToTopBtn.onclick = function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
 
 
 
